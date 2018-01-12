bba6f8f2cff9725900c0dfe996a8bee441a6b240
lingxiaohua <lingxiaohua@danlu.com>
2018-01-12 17:09:17 +0800

    是是是

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Routers from '../router.jsx';
+import header from '../src/header.jsx';
 //import logo from './logo.svg';
 //import './App.css';
 import {Link,Route,BrowserRouter as Router} from 'react-router-dom';
@@ -7,14 +8,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img  className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Routers />
+       <div class="header"><header /></div>
+        <div class="content">
+          <Routers />
+        </div>
       </div>
     );
   }
