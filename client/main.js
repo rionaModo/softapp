12e6bdb94290537afaa67e22779e4779f6fd892c
lingxiaohua <lingxiaohua@danlu.com>
2018-01-17 17:32:16 +0800

    对的

--- client/main.js
+++ client/main.js
@@ -5,7 +5,11 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import routes from './router'
 import App from './src/views/App.vue'
+import axios from 'axios'
 
+
+
+Vue.prototype.$http = axios
 window.Vue=Vue;
 Vue.use(VueRouter)
 const router = new VueRouter({
