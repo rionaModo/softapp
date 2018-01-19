0d32a55745ebc2f9519c57a09b1423b7b8b3323c
lingxiaohua <lingxiaohua@danlu.com>
2018-01-19 16:17:40 +0800

    学习数据库操作

--- server/lib/db/index.js
+++ server/lib/db/index.js
@@ -1,24 +1,11 @@
 /**
- * Created by riona on 2017/12/17.
+ *  链接数据库，对数据的操作来源cotroller
  */
 var mongoose=require('mongoose');
-
-
-
 mongoose.connect('mongodb://localhost:27017/softapp');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-/*var opendb=function(callback){
 
-  var Schema =  mongoose.Schema({name:String});
-  var users=mongoose.model('test',Schema,'users');
-  //var mongoModel = mongoose.model('test', Schema);
-  new users({name:'2018-1-19-2'}).save(function (err, fluffy) {
-    console.error(fluffy);
-    db.close()
-  })
-}
-opendb();*/
 module.exports=function(req,res,next){
   var params=Object.assign({},req.params)
   if(params&&params.c){
