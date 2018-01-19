937e41545f5e993bab7c064dc6b40eaaddf20d79
lingxiaohua <lingxiaohua@danlu.com>
2018-01-19 16:58:12 +0800

    学习数据库操作

--- server/lib/db/cotroller/user.js
+++ server/lib/db/cotroller/user.js
@@ -1,18 +1,14 @@
-/**
- * Created by danlu on 2018/1/18.
- */
+
 const mongoose=require('mongoose');
 var db = mongoose.connection;
-const user=require('../model/user');
 const action={
-  index:function(db,Schema){
+  index:function(Schema){
    var opx=function(req,res,next,call) {
   //  res.json(fluffy);
     return opx;
    }
   },
    userInfo:function(Schema){
-    console.log('Schema',Schema);
     var Schema = Schema||mongoose.Schema({});
     var users=mongoose.model('test',Schema,'users');
     var opx=function(req,res,next,call){
@@ -26,16 +22,6 @@ const action={
    }
 }
 
-module.exports=function(req,res,next,call){
- var params=Object.assign({},req.params)
- if(params.c&&params.a&&call&&action[params.a]){//根据数据库进行查询数据
-  var Schema=user[params.a]();
-  return action[params.a](Schema)(req,res,next,call);
- }else {
-  res.json({
-   status:-1,
-   msg:"没有对应的操作!"
-  });
- }
-}
+module.exports=action;
+
 
--- server/lib/db/index.js
+++ server/lib/db/index.js
@@ -7,10 +7,29 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
 module.exports=function(req,res,next){
-  var params=Object.assign({},req.params)
+  var params=Object.assign({},req.params);
   if(params&&params.c){
-    require('../db/cotroller/'+params.c)(req,res,next,function(data){
-      //console.log('mongodb handle is ok！');
-    })
+    const schemas=require('./model/'+params.c);
+    const cotrollers=require('./cotroller/'+params.c);
+    if(params.c&&params.a&&cotrollers[params.a]&&schemas[params.a]) {
+      var Schema = schemas[params.a]();
+      var cotroller=cotrollers[params.a]
+
+     cotroller(Schema)(req,res,next,function(data){
+       console.log('mongodb handle is ok！');
+     })
+      var m= cotroller(Schema);
+      console.log('cotroller',m);
+    }else {
+      res.json({
+        status:-1,
+        msg:"没有对应的操作!"
+      });
+    }
+  }else{
+    res.json({
+      status:-1,
+      msg:"没有对应的操作!"
+    });
   }
 }
